b9ef0b652938c6a6de09cf9a34e602bebdd8f51d
pavelberzkaln <p.berzkaln@gmail.com>
2022-03-20 18:34:31 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/oop/BallStory.java
@@ -0,0 +1,13 @@
+package oop;
+
+public class BallStory {
+    public static void main(String[] args) {
+        Fox fox = new Fox();
+        Wolf wolf = new Wolf();
+        Hare hare = new Hare();
+        Ball ball = new Ball();
+        hare.tryEat(ball);
+        wolf.tryEat(ball);
+        fox.tryEat(ball);
+    }
+}
