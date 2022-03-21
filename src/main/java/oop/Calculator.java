a2fd868cb80f6b6a872f05a235ae84e60ff18e5d
pavelberzkaln <p.berzkaln@gmail.com>
2022-03-21 22:08:39 +0300

    1.9. Статические и не статические методы [#504806]

---
+++ src/main/java/oop/Calculator.java
@@ -0,0 +1,32 @@
+package oop;
+
+public class Calculator {
+
+    private static int x = 5;
+
+    public static int sum(int y) {
+        return x + y;
+    }
+
+    public static int minus(int y) {
+        return  y - x;
+    }
+
+    public static int multiply(int y) {
+        return y * x;
+    }
+
+    public static int divide(int y) {
+        return y / x;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int rsl = calculator.sumAllOperation(10);
+        System.out.println(rsl);
+    }
+
+    public int sumAllOperation(int y) {
+        return sum(y) + multiply(y) + minus(y) + divide(y);
+    }
+}
