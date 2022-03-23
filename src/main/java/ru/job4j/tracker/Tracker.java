e3d2b5ed54ea92c4b040253c7b750ad2b4fac4d7
pavelberzkaln <p.berzkaln@gmail.com>
2022-03-23 22:47:37 +0300

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,9 +1,24 @@
 package ru.job4j.tracker;
 
+import javax.sound.midi.Track;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
+    private int id;
+    private String name;
+
+    public Tracker() {
+    }
+
+    public Tracker(String name) {
+        this.name = name;
+    }
+
+    public Tracker(int id) {
+        this.id = id;
+    }
 
     public Item add(Item item) {
         item.setId(ids++);
