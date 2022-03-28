08fec2500b25e82283e7b68b7a6d1ddc1030f87a
pavelberzkaln <p.berzkaln@gmail.com>
2022-03-28 22:50:58 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -16,16 +16,27 @@ public class Tracker {
         return item;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item name) {
+       int i = indexOf(id);
+       String tempName = name.getName();
+       items[i].setName(tempName);
+       return items[i].getName().equals(name.getName());
     }
 
     public Item[] findAll() {
