5aff2d77bae5b9581e4d3604d918358acaf00f2d
pavelberzkaln <p.berzkaln@gmail.com>
2022-04-05 23:48:27 +0300

    0.5. Иерархия исключений и множественный catch. [#219368]

---
+++ src/main/java/ex/UserStore.java
@@ -0,0 +1,36 @@
+package ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        for (User user : users) {
+            if (user.getUsername().equals(login)) {
+                return user;
+            }
+        }
+        throw new UserNotFoundException("The user wasn't found.");
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (user.isValid() && user.getUsername().length() > 3) {
+            return true;
+        }
+        throw new UserInvalidException("The user isn't valid.");
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException ie) {
+            ie.printStackTrace();
+        } catch (UserNotFoundException nf) {
+            nf.printStackTrace();
+        }
+        }
+}
