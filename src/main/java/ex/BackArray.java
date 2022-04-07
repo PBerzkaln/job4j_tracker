482847d4a66b1603ba53bf47361a1dcd1913ddbd
pavelberzkaln <p.berzkaln@gmail.com>
2022-04-07 20:49:42 +0300

    0. Что такое исключение. [#3821 #284563]

--- src/main/java/ex/BackArray.java
+++ src/main/java/ex/BackArray.java
@@ -7,10 +7,10 @@ public static void main(String[] args) {
         String[] names = {"Petr", "Ivan", "Nik", "Vasya"};
         int middle = names.length / 2;
         for (int index = 0; index < middle; index++) {
-        String temp = names[index];
-        names[index] = names[names.length - index - 1];
-        names[names.length - index - 1] = temp;
-        }
+                String temp = names[index];
+                names[index] = names[names.length - index - 1];
+                names[names.length - index - 1] = temp;
+                }
         System.out.println(Arrays.toString(names));
         }
 }
