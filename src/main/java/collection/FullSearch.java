fc353807c1178c0ac1e2880acd437dec2076434a
pavelberzkaln <p.berzkaln@gmail.com>
2022-04-18 22:09:13 +0300

    1. Уникальные задачи. [#212653]

---
+++ src/main/java/collection/FullSearch.java
@@ -0,0 +1,14 @@
+package collection;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class FullSearch {
+    public static HashSet<String> extractNumber(List<Task> list) {
+        HashSet<String> numbers = new HashSet<>();
+        for (Task lists : list) {
+            numbers.add(lists.getNumber());
+        }
+        return numbers;
+    }
+}
