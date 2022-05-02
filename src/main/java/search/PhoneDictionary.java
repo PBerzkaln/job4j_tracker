9a05d08cc5833d7697713c4471df31df8cc736a6
pavelberzkaln <p.berzkaln@gmail.com>
2022-05-02 23:38:20 +0300

    6. Функции высшего порядка [#24255]

--- src/main/java/search/PhoneDictionary.java
+++ src/main/java/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<>();
@@ -10,18 +11,22 @@ public class PhoneDictionary {
     }
 
     /**
-     * Вернуть список всех пользователей, который содержат key в любых полях.
-     * @param key Ключ поиска.
-     * @return Список подощедщих пользователей.
+     * Возвращает список всех пользователей, которые содержат key в любых полях.
+     * @param key ключ поиска.
+     * @return Список подошедщих пользователей.
      */
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
+        Predicate<Person> byPhone = (i) -> i.getPhone().contains(key);
+        Predicate<Person> byAddress = (i) -> i.getAddress().contains(key);
+        Predicate<Person> byName = (i) -> i.getName().contains(key);
+        Predicate<Person> bySurname = (i) -> i.getSurname().contains(key);
+        Predicate<Person> combine = byPhone.or(byAddress).or(byName).or(bySurname);
         for (Person person : persons) {
-        if (person.getPhone().contains(key) || person.getAddress().contains(key)
-        || person.getName().contains(key) || person.getSurname().contains(key)) {
-            result.add(person);
-        }
+            if (combine.test(person)) {
+                result.add(person);
             }
+        }
         return result;
     }
 }
--- src/test/java/search/PhoneDictionaryTest.java
+++ src/test/java/search/PhoneDictionaryTest.java
@@ -16,15 +16,4 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
-
-    @Test
-    public void whenFindByNameFalls() {
-        PhoneDictionary phones = new PhoneDictionary();
-        ArrayList<Person> rsl = new ArrayList<>();
-        phones.add(
-                new Person(" ", "Arsentev", "534872", "Bryansk")
-        );
-        ArrayList<Person> persons = phones.find("Petr");
-        assertThat(persons, is(rsl));
-    }
 }
